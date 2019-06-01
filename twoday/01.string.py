a1819452d3741babba15d079cf89890b7e6e8de6
ziyins <ziyins@163.com>
2019-06-01 10:33:58 +0800

    string

--- twoday/01.string.py
+++ twoday/01.string.py
@@ -16,7 +16,10 @@ variableLen = len(variable)     # 求字符串的长度
 for i in range(variableLen):
     print(variable[i])
 
+# 遍历字符串, 打印字符串中的单个字符.
+print("第11索引值为: ", variable[11])
 
-
-
-
+# range问题(左闭右开区间).所取数值均为整数.
+# 左闭右开区间 = [1...100) i先取“1”, ...., i取到99时.区间结束.
+# range(1, 100): 1 <= i <= 99
+# range(100): 0 <= i <= 99
