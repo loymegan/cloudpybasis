2a6dee90a6d55216dbb082b54c30104ece2d6cf5
ziyins <ziyins@163.com>
2019-06-01 10:13:37 +0800

    string

---
+++ twoday/01.string.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+# author: bavdu
+# email: bavduer@163.com
+# date: 2019/06/01
+# usage: study string.
+
+
+# 字符串类型的变量定义.
+variable = "this my girl friends."
+
+# t h i s   m y   g i r  l     f  r  i  e  n  d  s  .
+# 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
+
+# 遍历字符串
+variableLen = len(variable)     # 求字符串的长度
+for i in range(variableLen):
+    print(variable[i])
+
+
+
+
+
