9e3f9ee649db9f7a6e7ba14f1fa880cc81d791e5
ziyins <ziyins@163.com>
2019-06-04 16:40:55 +0800

    exam

--- threeday/fileio.py
+++ threeday/fileio.py
@@ -3,6 +3,8 @@
 # date: 2019/06/04
 # usage: file output info
 
+
+# 第一道题: 把文件中的信息输出到下面的字典中
 information = {
     "China":
         {
@@ -25,5 +27,21 @@ information = {
 }
 
 
+# 第二道题: 取到"1T"
+info = []
+info.append(
+    {'192.168.1.11':
+         {'cpu': ['Intel(R) Core(TM) i5-5350U CPU @ 1.80GHz', 4, 1],
+          'memory': ['16', '4', '2'],
+          'disk': ['1T', '2T']
+         }
+    })
 
+info.append({
+    '192.168.1.12': {
+         'cpu': ['Intel(R) Core(TM) i5-5350U CPU @ 1.80GHz', 4, 1],
+         'memory': ['16', '4', '2'],
+         'disk': ['1T', '2T']
+    }
+})
 
