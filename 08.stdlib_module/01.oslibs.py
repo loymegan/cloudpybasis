cfe648ad19355be503b0dfc1fc600d7292a3f4c8
ziyins <ziyins@163.com>
2019-06-11 15:39:24 +0800

    os module

--- 08.stdlib_module/01.oslibs.py
+++ 08.stdlib_module/01.oslibs.py
@@ -10,10 +10,30 @@ abspath = os.path.abspath("01.oslibs.py")
 print(abspath)
 
 # 执行shell命令
-if os.system("ls /User") == 0:
-    os.system("yum -y install httpd")
-else:
-    print("Failed")
+# if os.system("ls /User") == 0:
+#     os.system("yum -y install httpd")
+# else:
+#     print("Failed")
+
+# 文件的增删改查
+# os.mkdir("/Users/chaoliu/Documents/osmkdir", 0o777)
+# os.rmdir("/Users/chaoliu/Documents/osmkdir")
+os.chdir("/Users/chaoliu/Documents")
+path = os.getcwd()
+print(path)
 
 
+# t = os.open("git.txt", os.O_RDWR | os.O_CREAT)
+# b = str.encode("uuuu")
+# os.write(t, b)
+# os.close(t)
+
+
+# 查看目录中的内容,并把目录中的内容返回一个列表
+t = os.listdir("/Users/chaoliu/Documents")
+for i in t:
+    print(i)
+
+# remove需要权限
+os.remove("/Users/chaoliu/Documents/uuuu")
 
