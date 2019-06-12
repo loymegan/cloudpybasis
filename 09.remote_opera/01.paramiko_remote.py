9a44ca503acedda15c227c7d3024904cc9e7cb3f
ziyins <ziyins@163.com>
2019-06-12 16:46:21 +0800

    paramiko module

--- 09.remote_opera/01.paramiko_remote.py
+++ 09.remote_opera/01.paramiko_remote.py
@@ -10,11 +10,11 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect(hostname='39.100.110.135', port=22, username='root', password='')
 
 while True:
-    input_command = input('>>>: ')
-    if input_command == 'quit':
+    command = input('>>>: ')
+    if command == 'quit':
         break
     # 执行命令，输出结果在stdout中，如果是错误则放在stderr中
-    stdin, stdout, stderr = ssh.exec_command(input_command)
+    stdin, stdout, stderr = ssh.exec_command(command)
     result = stdout.read()  # read方法读取输出结果
     if len(result) == 0:    # 判断如果输出结果长度等于0表示为错误输出
         print(stderr.read())
--- 09.remote_opera/opera02.py
+++ 09.remote_opera/opera02.py
@@ -1,1 +1,24 @@
+import paramiko
 
+# 生成一个连接远程机器的客户端
+ssh = paramiko.SSHClient()
+
+# 加入主机列表中
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+# 建立连接
+ssh.connect(hostname='39.100.110.135', port=22, username='root', password='liuchao.0725')
+
+while True:
+    command = input('[root@39.100.110.135 ~]# ')
+    if command == 'exit' or command == 'quit':
+        break
+    stdin, stdout, stderr = ssh.exec_command(command, get_pty=True)
+    result = stdout.read()
+    print(str(result, 'utf-8'))
+    if len(result) == 0:
+        print(str(stderr.read(), 'utf-8'))
+    else:
+        print(str(result, 'utf-8'))
+
+ssh.close()
